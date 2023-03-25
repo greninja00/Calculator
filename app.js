bd18b7e6798b37b9dbc255703fa1d4ad2a802ff3
@greninja00 <12deep2001@gmail.com>
2023-03-25 21:34:25 +0530

    complete the model and styling the calculator

---
+++ app.js
@@ -0,0 +1,24 @@
+//basic defination of operations
+function add(a, b)
+{
+    return a+b;
+}
+function subtract(a, b)
+{
+    return a-b;
+}
+function divide(a, b)
+{
+    return a/b;
+}
+function multiply(a, b)
+{
+    return a*b;
+}
+
+//defining the variables use to update the screen of the calculator
+let firstnumber;
+let secondnumber;
+let operator;
+
+
